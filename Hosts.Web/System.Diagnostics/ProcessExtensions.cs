13033840217e80baff317fb69ede4f99089ebfe9
MALU <contact@andantissimo.jp>
2021-12-15 07:55:58 +0900

    fix

--- Hosts.Web/System.Diagnostics/ProcessExtensions.cs
+++ Hosts.Web/System.Diagnostics/ProcessExtensions.cs
@@ -5,17 +5,17 @@ internal static class ProcessExtensions
     public static async Task<int> StartAsync(this Process process, CancellationToken cancellationToken = default)
     {
         var exited = new TaskCompletionSource<int>();
+        process.EnableRaisingEvents = true;
+        process.Exited += (_, _) => exited.TrySetResult(process.ExitCode);
+        process.Start();
+        if (process.HasExited)
+            return process.ExitCode;
         using var _ = cancellationToken.Register(() =>
         {
             if (process.Id != default)
                 process.Kill(true);
             exited.TrySetCanceled(cancellationToken);
         });
-        process.EnableRaisingEvents = true;
-        process.Exited += (_, _) => exited.TrySetResult(process.ExitCode);
-        process.Start();
-        if (process.HasExited)
-            return process.ExitCode;
         return await exited.Task.ConfigureAwait(false);
     }
 }
